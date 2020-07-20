df414ee032c89cdc3fa59a6e1b8f999990f0b354
davidjbuchanan <davidjbuchanan@gmail.com>
2020-07-20 11:19:12 +0000

    Added canonical URL

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
-class PublishedManager(models.Manager):
+class PublishedManager(models.Manager):  # Generation of a custom manager.
     def get_queryset(self):
         return super(PublishedManager,
                      self).get_queryset()\
@@ -28,6 +29,13 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+
+    def get_absolute_url(self):  # Generation of a canonical URL.
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
     class Meta:
         ordering = ('-publish',)
