21cff9b8070584792512e8101d8e901fa41a2093
davidjbuchanan <davidjbuchanan@gmail.com>
2020-07-20 14:45:59 +0000

    Added views,URLs and template for blog app

--- ml4_djb/urls.py
+++ ml4_djb/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
+    path('blog/', include('blog.urls', namespace='blog')),  # use of namespace - to be reviewed i.e. extended to other paths or deleted
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
