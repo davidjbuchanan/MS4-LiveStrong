c402dc1f427267a8a119d774e365a62b6f237cd7
davidjbuchanan <davidjbuchanan@gmail.com>
2020-08-01 09:02:33 +0000

    Added meta class

--- coupons/forms.py
+++ coupons/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from .models import Coupon
 
 
 class CouponApplyForm(forms.Form):
     code = forms.CharField()
+
+    class Meta:
+        model = Coupon
+        fields = (
+            'code',
+        )
