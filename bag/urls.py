3fe762f567d074de2967c984d0c9568ff6c1ff1f
davidjbuchanan <davidjbuchanan@gmail.com>
2020-08-02 19:00:37 +0000

    Added adjust/remove bag views and calc subtotal template filter

--- bag/urls.py
+++ bag/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
         path('', views.view_bag, name='view_bag'),
         path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
         path('apply/', views.coupon_apply, name='apply'),
+        path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
+        path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
