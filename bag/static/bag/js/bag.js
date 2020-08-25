9d72653a5c3774428eb63beaaf75f2bb669ec1dc
davidjbuchanan <davidjbuchanan@gmail.com>
2020-08-25 12:58:34 +0000

    Added JQ and styling to coupon form

--- bag/static/bag/js/bag.js
+++ bag/static/bag/js/bag.js
@@ -7,5 +7,27 @@ $(document).ready(function() {
         this.style.setProperty( "border", "thin solid transparent");
         this.style.setProperty("color", "white");
     });
-});
+    $("#submit_code").on("click",function() {           
+        $("#submit_code").hide();
+        $("#coupon_form").show();
+    });
+    $("#cancel").on("click",function() {        
+        $("#coupon_form").hide();
+        $("#submit_code").show();
+    });
+    $(".update-link").click(function(e) {
+        var form = $(this).prev('.update-form');
+        form.submit();
+    });
+    $(".remove-item").click(function(e) {
+        var csrfToken = "{{ csrf_token }}";
+        var itemId = $(this).attr('id').split('remove_')[1];
+        var url = `/bag/remove/${itemId}/`;
+        var data = {'csrfmiddlewaretoken': csrfToken};
 
+        $.post(url, data)
+         .done(function() {
+             location.reload();
+         });
+    });
+});    
--- coupons/forms.py
+++ coupons/forms.py
@@ -10,3 +10,22 @@ class CouponApplyForm(forms.Form):
         fields = (
             'code',
         )
+
+#  all new beneath this line
+
+    def __init__(self, *args, **kwargs):
+            """
+            Add placeholders and classes, remove auto-generated
+            labels and set autofocus on first field
+            """
+            super().__init__(*args, **kwargs)
+            placeholders = {
+                'code': 'Enter coupon code',
+            }
+
+            self.fields['code'].widget.attrs['autofocus'] = True
+            for field in self.fields:
+                placeholder = f'{placeholders[field]}'
+
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].label = False
