92d8fb2e29888859e30cc8cfa85ae2f63ca377d3
davidjbuchanan <davidjbuchanan@gmail.com>
2020-09-15 09:06:15 +0000

    Added 404 page and links

--- home/urls.py
+++ home/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index, name='home')
+        path('', views.index, name='home'),
+        path('under_construction', views.index_404, name='under_construction'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def index_404(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/index_404.html')
