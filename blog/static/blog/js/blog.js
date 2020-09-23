c3cdb720aec6813f8d0f543a7cf8941fe7d95bfd
davidjbuchanan <davidjbuchanan@gmail.com>
2020-09-23 16:41:46 +0000

    Deleted whitespace

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $(".btn-black").mouseenter(function() {               
+    $(".btn-black").mouseenter(function() {       
         this.style.setProperty("border", "thin solid red");
         this.style.setProperty("color", "red");
     });
-    $(".btn-black").mouseleave(function() {               
+    $(".btn-black").mouseleave(function() {
         this.style.setProperty( "border", "thin solid transparent");
         this.style.setProperty("color", "white");
     });
