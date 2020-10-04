dbfd86ae2c6a858d688f3dd9d6017ece28df2461
davidjbuchanan <davidjbuchanan@gmail.com>
2020-10-04 16:59:01 +0000

    Moved the 'remove' JQuery function into html page

--- bag/static/bag/js/bag.js
+++ bag/static/bag/js/bag.js
@@ -19,14 +19,4 @@ $(document).ready(function() {
         var form = $(this).prev('.update-form');
         form.submit();
     });
-    $(".remove-item").click(function(e) {
-        var csrfToken = "{{ csrf_token }}";
-        var itemId = $(this).attr('id').split('remove_')[1];
-        var url = `/bag/remove/${itemId}/`;
-        var data = {'csrfmiddlewaretoken': csrfToken};
-        $.post(url, data)
-         .done(function() {
-             location.reload();
-         });
-    });
 });
